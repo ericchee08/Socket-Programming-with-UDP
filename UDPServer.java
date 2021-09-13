17c5404387f6e7ba88cb5bb7303ee10894955b8b
Eric Chee <58150120+ericchee08@users.noreply.github.com>
2021-09-13 20:16:46 +0100

    Add files via upload

--- UDPServer.java
+++ UDPServer.java
@@ -6,22 +6,32 @@ import java.net.*;
 class UDPServer {
 public static void main(String args[]) throws Exception
 {
+	// Create serverSocket = 9876 for incoming request: 
 	DatagramSocket serverSocket = new DatagramSocket(9876);
 	byte[] receiveData = new byte[1024];
 	byte[] sendData = new byte[1024];
-while(true)
-{	
+	while(true)
+{
 	System.out.println("Server Running");
+
 	DatagramPacket receivePacket = new DatagramPacket(receiveData,
 	receiveData.length);
+
+	// Read request from serverSocket
 	serverSocket.receive(receivePacket);
 	String sentence = new String(receivePacket.getData());
 	InetAddress IPAddress = receivePacket.getAddress();
 
+	// getPort() method of java Socket class returns the remote port number to which the socket is connected.
+	// allowing to write a reply back to the client
 	int port = receivePacket.getPort();
 	System.out.println("Received: " + sentence);
+	
+	//perform capitalization function
 	String capitalizedSentence = sentence.toUpperCase();
 	sendData = capitalizedSentence.getBytes();
+
+	// write reply to serverSocket specifying client host IP address and port number
 	DatagramPacket sendPacket = new DatagramPacket(sendData,
 	sendData.length, IPAddress, port);
 	serverSocket.send(sendPacket);
